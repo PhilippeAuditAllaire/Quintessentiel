6c133df05a6e78bff100a9cd3711fc9bbf666a80
PhilippeAuditAllaire <warpaintingqc@gmail.com>
2019-11-14 01:54:02 -0500

    Jai le gout de me suicider a cause de ste query la
    Ca lair que ca marche pas pis que je suis pas capable de juste avoir les produit qui ont EXCATEMENT les categories qui me faut

--- serverSide/managers/MgrProduct.js
+++ serverSide/managers/MgrProduct.js
@@ -187,14 +187,18 @@ class MgrProduct {
     }
 
     loadProductSearchCategory(code_lang, search) {
-        let query = "SELECT Product.id as product_id, productattribute.*, ta_productattribute_language.*, Product.image as image, Product.retailPrice FROM product INNER JOIN ta_productattribute_language ON Product.id = ta_productattribute_language.idProduct INNER JOIN productattribute ON ta_productattribute_language.productAttributeId = productattribute.id INNER JOIN ta_category_product ON product.id = ta_category_product.idProduct INNER JOIN category ON ta_category_product.idCategory = category.id INNER JOIN ta_categoryattribute_language ON category.id = ta_categoryattribute_language.idCategory WHERE ta_categoryattribute_language.value='" + search[0] + "'";
-        if(search.length>1){
-            for(let i=1;i<search.length;i++){
-                let conditions="AND ta_categoryattribute_language.value='" + search[i] + "'"
+        let query = "SELECT Product.id as product_id, productattribute.*, ta_productattribute_language.*, Product.image as image, Product.retailPrice FROM product INNER JOIN ta_productattribute_language ON Product.id = ta_productattribute_language.idProduct INNER JOIN productattribute ON ta_productattribute_language.productAttributeId = productattribute.id INNER JOIN ta_category_product ON product.id = ta_category_product.idProduct INNER JOIN category ON ta_category_product.idCategory = category.id INNER JOIN ta_categoryattribute_language ON category.id = ta_categoryattribute_language.idCategory WHERE ta_categoryattribute_language.value IN (";
+            for(let i=0;i<search.length;i++){
+                console.log("I : "+i);
+                console.log("Search lenght: "+search.length);
+                if(i!=(search.length-1)){
+                    var conditions="'" + search[i] +"',";
+                }else{
+                    var conditions="'" + search[i] +"'";
+                }  
                 query=query.concat(conditions);
-            }
         }
-        let endQuery=" GROUP BY ta_category_product.id";
+        let endQuery=") GROUP BY Product.id";
         query=query.concat(endQuery);
         
         console.log(query);
