1f5a24c1d2d29f6b90ed628084ff6ac8ee83c3b7
DavidGau <gaulindavidweb@gmail.com>
2019-11-19 20:27:11 -0500

    appStart.js window show only when ready

--- appStart.js
+++ appStart.js
@@ -6,18 +6,21 @@ require('electron-reload')(__dirname);
 //Create the main window
 function createWindow () {
 
-  // Creating the window object
+  //Creating the window object
   mainWindow = new BrowserWindow({
     webPreferences: {nodeIntegration: true},
     icon:'./public/images/logoAppNav.png',
     width: 800,
     height: 600,
-    frame:false,
+    frame: false,
     'minHeight': 500,
-    'minWidth': 550 
+    'minWidth': 550,
+    show: false
   });
 	
-	
+	//Show the window when ready
+  mainWindow.once("ready-to-show",() => mainWindow.show());
+
   //Redirect to the URL
   mainWindow.loadURL('http://localhost:5000/')
 }
