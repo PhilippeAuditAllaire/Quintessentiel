f0f77769b12cc967e493c993b9cf5a4337c5cf31
DavidGau <gaulindavidweb@gmail.com>
2019-11-19 20:22:11 -0500

    Cleaned appStart.js

--- appStart.js
+++ appStart.js
@@ -3,25 +3,23 @@ const nativeImage = require('electron').nativeImage;
 
 require('electron-reload')(__dirname);
 
+//Create the main window
 function createWindow () {
 
-	var image = nativeImage.createFromPath(__dirname + '/public/images/logoApp2.png'); 
-  // Creating the window
-  win = new BrowserWindow({webPreferences: {nodeIntegration: true},  icon:image,width: 800, height: 600, frame:false,'minHeight': 500,'minWidth': 550})
+  // Creating the window object
+  mainWindow = new BrowserWindow({
+    webPreferences: {nodeIntegration: true},
+    icon:'./public/images/logoAppNav.png',
+    width: 800,
+    height: 600,
+    frame:false,
+    'minHeight': 500,
+    'minWidth': 550 
+  });
 	
 	
-  //Load the file
-  win.loadURL('http://localhost:5000/')
-
-  win.on("new-window", function(event, url) {
-    event.preventDefault();
-    shell.openExternal(url);
-  });
-
-  ipcMain.on('resize-window', (event, size) => {
-    win.setSize(size.width,size.height);
-  });
-
+  //Redirect to the URL
+  mainWindow.loadURL('http://localhost:5000/')
 }
   
   app.on('ready', createWindow)
