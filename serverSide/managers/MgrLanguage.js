596661010d01bc0c295a8e244ca2e3533d95d47c
CatherineBronsard <cbronsard@videotron.ca>
2019-11-20 23:28:36 -0500

    oups forgot mgrLanguage

---
+++ serverSide/managers/MgrLanguage.js
@@ -0,0 +1,30 @@
+const QueryEngine = require("../scripts/QueryEngine.js");
+
+class MgrLanguage {
+
+    constructor() {
+        this._queryEngine = new QueryEngine();
+    }
+    getTextByPage(page, code_lang) {
+        let query = "SELECT landingtext_attribute.type,ta_landingtextattribute_language.value FROM ta_landingtextattribute_language JOIN landingtext_attribute ON landingtext_attribute.id = ta_landingtextattribute_language.landingTextAttribute JOIN language ON ta_landingtextattribute_language.languageId = language.id WHERE language.name = ? AND landingtext_attribute.page = ?";
+        let parameters = [code_lang, page];
+        let context = this;
+        return this._queryEngine.executeQuery(query, parameters).then(function(res) {
+
+            return context.toJSon(res);
+        });
+    }
+
+    toJSon(resultat) {
+        let json = '{';
+        resultat.forEach(function(line) {
+            json += '"' + line.type + '":' + '"' + line.value + '",';
+        });
+        let res = json.substring(0, json.length - 1);
+        res += '}';
+        console.log("JSON : " + res);
+        return res;
+    }
+}
+
+module.exports = MgrLanguage;
