48ec64bda1885416f0affdb3e4735714fa7c4ecf
DavidGau <gaulindavidweb@gmail.com>
2019-11-27 14:14:05 -0500

    Populated DB with provinces and countries

--- sql/quintessentielDb.sql
+++ sql/quintessentielDb.sql
@@ -342,25 +342,7 @@ CREATE TABLE TA_FAQQuestionAttribute_Language(
 	PRIMARY KEY(idQuestionAttribute,idLanguage)
 );
 
-CREATE TABLE Province(
-	id SMALLINT AUTO_INCREMENT PRIMARY KEY
-);
 
-CREATE TABLE ProvinceAttribute(
-	id SMALLINT AUTO_INCREMENT PRIMARY KEY,
-	type TEXT NOT NULL
-);
-
-CREATE TABLE TA_ProvinceAttribute_Language(
-	provinceAttributeId SMALLINT NOT NULL,
-	FOREIGN KEY (provinceAttributeId) REFERENCES ProvinceAttribute(id),
-	idLanguage SMALLINT NOT NULL,
-	FOREIGN KEY (idLanguage) REFERENCES Language(id),
-	idProvince SMALLINT NOT NULL,
-	FOREIGN KEY (idProvince) REFERENCES Province(id),
-	value TEXT,
-	PRIMARY KEY(provinceAttributeId,idLanguage,idProvince)
-);
 
 CREATE TABLE Country(
 	id SMALLINT AUTO_INCREMENT PRIMARY KEY
@@ -382,3 +364,24 @@ CREATE TABLE TA_CountryAttribute_Language(
 	PRIMARY KEY(countryAttributeId,idLanguage,idCountry)
 );
 
+CREATE TABLE Province(
+	id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+	idCountry SMALLINT NOT NULL,
+	FOREIGN KEY (idCountry) REFERENCES Country(id)
+);
+
+CREATE TABLE ProvinceAttribute(
+	id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+	type TEXT NOT NULL
+);
+
+CREATE TABLE TA_ProvinceAttribute_Language(
+	provinceAttributeId SMALLINT NOT NULL,
+	FOREIGN KEY (provinceAttributeId) REFERENCES ProvinceAttribute(id),
+	idLanguage SMALLINT NOT NULL,
+	FOREIGN KEY (idLanguage) REFERENCES Language(id),
+	idProvince SMALLINT NOT NULL,
+	FOREIGN KEY (idProvince) REFERENCES Province(id),
+	value TEXT,
+	PRIMARY KEY(provinceAttributeId,idLanguage,idProvince)
+);
