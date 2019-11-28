9697525f4b3dc0c61f6c95004bd28586945ac7ae
CatherineBronsard <cbronsard@videotron.ca>
2019-11-28 00:22:27 -0500

    promos fonctionnent

--- serverSide/managers/MgrPromotion.js
+++ serverSide/managers/MgrPromotion.js
@@ -17,6 +17,11 @@ class MgrPromotion {
         return this._queryEngine.executeQuery(query, param);
     }
 
+    loadAllPromos() {
+        let query = "SELECT * FROM promo JOIN ta_productattribute_language ON ta_productattribute_language.idProduct = promo.id_product JOIN productattribute ON productattribute.id = ta_productattribute_language.productAttributeId WHERE productattribute.type = 'title' AND ta_productattribute_language.idLanguage = 1";
+        return this._queryEngine.executeQuery(query);
+    }
+
 }
 
 module.exports = MgrPromotion;
